7daa73dcbff9d55257df7686eb915e3ff4e6d092
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-01-01 10:46:18 +0800

    v1.0.0 适配2026年1月签到活动。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/__init__.py
+++ module/__init__.py
@@ -21,7 +21,7 @@ console = Console(log_path=False, log_time_format=LOG_TIME_FORMAT)
 AUTHOR = 'Gentlesprite'
 __version__ = '1.0.0'
 __license__ = 'MIT License'
-__update_date__ = '2025/10/03 12:08:40'
+__update_date__ = '2026/01/01 10:45:54'
 __copyright__ = f'Copyright (C) 2024-{__update_date__[:4]} {AUTHOR} <https://github.com/Gentlesprite>'
 SOFTWARE_FULL_NAME = 'NZ_Helper_Sign'
 SOFTWARE_SHORT_NAME = 'NHS'
