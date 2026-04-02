f127a74deabd6dbb12e09dd363f332a333863384
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-04-02 14:39:36 +0800

    v1.0.0 适配4月累计签到礼包和限定日期礼包。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ activity_id: str = '885966'
 flow_id: str = '1197212'
 sd_id: str = ''.join(random.choices('0123456789abcdef', k=32))
 special_date: list = ['2026-04-01', '2026-04-10', '2026-04-17', '2026-04-24']
-special_date_flow_id: str = '1192661'
+special_date_flow_id: str = '1197216'
 cumulative_day: list = [3, 5, 9, 15, 20, 25]
-cumulative_day_flow_id: str = '1192656'
+cumulative_day_flow_id: str = '1197211'
