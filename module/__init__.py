696ca4382dd6a6d949cd9d3fb51bea0ff55d4207
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2026-04-09 15:11:44 +0800

    v1.0.0 版本福利没有领取成功时不再进行通知。 v1.0.0 修复版本福利抽奖最后一次仍然延时的问题。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/__init__.py
+++ module/__init__.py
@@ -26,7 +26,7 @@ console = Console(log_path=False, log_time_format=LOG_TIME_FORMAT)
 AUTHOR = 'Gentlesprite'
 __version__ = '1.0.0'
 __license__ = 'MIT License'
-__update_date__ = '2026/04/09 09:20:27'
+__update_date__ = '2026/04/09 15:11:38'
 __copyright__ = f'Copyright (C) 2024-{__update_date__[:4]} {AUTHOR} <https://github.com/Gentlesprite>'
 SOFTWARE_FULL_NAME = 'NZ_Helper_Sign'
 SOFTWARE_SHORT_NAME = 'NHS'
